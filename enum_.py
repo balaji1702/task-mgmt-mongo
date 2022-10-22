3a2e178da9019f2a3e06e548e549535a1186a6de
balaji1702 <balajichandrasekar17@gmail.com>
2022-10-23 00:37:41 +0530

    unwanted

--- enum_.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-class Priority(Enum):
-    High="HIGH PRIORITY"
-    Low="LOW PRIORITY"
-class Status(Enum):
-    pending="PENDING"
-    completed="COMPLETED"
--- schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from enum_ import Priority,Status
-
-
-class Task(BaseModel):
-    task_name: str
-    description: str  
-    priority :Priority
-    status: Status
